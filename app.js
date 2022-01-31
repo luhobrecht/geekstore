941b93440b1da20ed9478a55d2a37c061c20aca2
luhobrecht <luhobrecht@hotmail.com>
2022-01-31 21:26:05 +0100

    Agrego archivo productDetail.html y estilos correspondientes en css

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.use(express.static('public'));
+
+
+app.listen(3000, () => {
+     console.log("Funciona todo bien");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+})
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"));
+})
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/productCart.html"));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+})
