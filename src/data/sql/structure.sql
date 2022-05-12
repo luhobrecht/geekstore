f2b702a007c92c130cd5957f46b133df53a03217
luhobrecht <luhobrecht@hotmail.com>
2022-05-12 14:07:02 +0200

    Corrección de base de datos y carga de datos en la base

--- src/data/sql/structure.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE `geek_store`
-
-CREATE TABLE `products` 
-( `id` INT NOT NULL , `name` VARCHAR NOT NULL , `price` FLOAT NOT NULL , `description` TEXT NOT NULL , `category` VARCHAR NOT NULL , `colors` VARCHAR NOT NULL , `size` VARCHAR NOT NULL , `discount` INT NOT NULL , `type` VARCHAR NOT NULL ,
- `img` VARCHAR NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
